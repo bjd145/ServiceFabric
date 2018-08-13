145a690281434dcf78f14de016a488c347117cbd
Brian Denicola <bjd145@yahoo.com>
2018-08-13 10:40:31 -0500

    Code to setup clusters

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ServiceFabric
+This Repo contains code to setup a Service Fabric cluster either on-premise or in Azure.  
+It has two sample application package to deploy
+1. The Same WordCount applcation that demos Stateless and Stateful native SF applications
+2. A very simple docker-compose file to deploy the default microsoft/iis docker Windows container
---
+++ WordCount/Deploy.ps1
@@ -0,0 +1,24 @@
+param(
+    [string] $SFCluster,  #xyzcontainers.westus.cloudapp.azure.com:19000
+    [string] $Package,
+    [string] $ApplicationName,
+    [string] $ServerCert,
+    [string] $AdminCert,
+    [switch] $Insecure
+)
+
+if($Insecure) {
+    Connect-ServiceFabricCluster -ConnectionEndpoint $SFCluster
+}
+else {
+    Connect-ServiceFabricCluster -ConnectionEndpoint $SFCluster `
+        -X509Credential -ServerCertThumbprint $ServerCert `
+        -FindType FindByThumbprint -FindValue $AdminCert `
+        -StoreLocation CurrentUser -StoreName 'My'
+}
+
+$opts = @{
+    ApplicationPackagePath =  (Get-Item $Package | Select-Object -ExpandProperty FullName)
+    ApplicationName = ("fabric:/{0}" -f $ApplicationName)
+}
+Publish-NewServiceFabricApplication @opts
